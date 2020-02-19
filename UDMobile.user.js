26dd3eeb5131773292dccd39653f75ce1ddbac58
defufna <defufna@gmail.com>
2020-02-19 14:41:06 +0100

    Added viewport tag

--- UDMobile.user.js
+++ UDMobile.user.js
@@ -10,27 +10,33 @@
 
 
 function mobileView(){
-    var target = document.querySelector("div.gthome");
-    var div = document.createElement("div");
-    div.setAttribute("class", "gp");
-    
-    var source = document.querySelector("td.gp");
-    while(source.children.length > 0){
-      div.appendChild(source.children[0]);
-    }
-    source.remove();
-    target.insertAdjacentElement('afterend', div);
-    
-    map = document.querySelector("table.c");
-    
-    style = map.getAttribute("style");
-    if(style == null){
-      style = "";
-    }
-    
-    style += "margin:auto;"
-    map.setAttribute("style", style);
-    
+  var viewPortTag=document.createElement('meta');
+  viewPortTag.name = "viewport";
+  viewPortTag.content = "width=480, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
+  document.querySelector('head').appendChild(viewPortTag);
+ 
+  
+  var target = document.querySelector("div.gthome");
+  var div = document.createElement("div");
+  div.setAttribute("class", "gp");
+  
+  var source = document.querySelector("td.gp");
+  while(source.children.length > 0){
+    div.appendChild(source.children[0]);
+  }
+  source.remove();
+  target.insertAdjacentElement('afterend', div);
+  
+  map = document.querySelector("table.c");
+  
+  style = map.getAttribute("style");
+  if(style == null){
+    style = "";
   }
   
-  mobileView();
+  style += "margin:auto;"
+  map.setAttribute("style", style);
+  
+}
+
+mobileView();
